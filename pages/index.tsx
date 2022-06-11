8916786ef277455037eccf1f76e1f6502b8e6f1e
Victor Oliveira <victoroliveiraspk@gmail.com>
2022-06-11 17:29:39 -0300

    shows random fact phrase and refresh button

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,15 @@ const Home: NextPage = () => {
         <Image src='/chuck-norris-push-up.gif' width={400} height={300}/>
         <input className='shadow appearance-none border rounded w-6/12 py-4 px-3 text-gray-700 focus:outline-none absolute' placeholder='Search...' style={{ bottom: '-25px' }}/>
       </form>
+
+      <div className='flex justify-center'>
+        <div className='bg-gray-200 w-6/12 mt-24 p-2'>
+          <span className='text-gray-500 text-3xl'>"</span>
+          <p className='text-center'>It is possible to contract hepatitis from smelling one of Chuck Norris's farts.</p>
+          <p className='text-gray-500 text-3xl text-right'>"</p>
+          <button className='text-xs p-2 text-white round rounded-xl' style={{ backgroundColor: 'rgba(155,109,162,255)' }}>Update random fact</button>
+        </div>
+      </div>
     </>
   )
 }
