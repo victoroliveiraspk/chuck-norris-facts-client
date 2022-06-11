8210cdbf8f0755b354c11447d0b5eabec0bc0a2c
Victor Oliveira <victoroliveiraspk@gmail.com>
2022-06-11 20:00:11 -0300

    show loading indicator in RandomFact component

--- src/components/RandomFact.tsx
+++ src/components/RandomFact.tsx
@@ -18,18 +18,23 @@ const RandomFact = () => {
   useEffect(() => {
     updateRandomFact()
   }, [])
-  
+
   return (
     <div className='flex justify-center'>
-      <div className='bg-gray-200 w-6/12 p-2'>
-        <span className='text-gray-500 text-3xl'>"</span>
-        <p className='text-center'>{fact}</p>
-        <p className='text-gray-500 text-3xl text-right'>"</p>
-        <div className='flex justify-center'>
-          <button className='text-xs p-2 text-white round rounded-xl' style={{ backgroundColor: 'rgb(155,109,162)' }} onClick={updateRandomFact}>
-            Update random fact
-          </button>
-        </div>
+      <div className='bg-gray-200 w-full p-2 h-52 flex flex-col justify-around items-center'>
+       {loading && <div className='loading'></div>}
+        
+        {loading || (
+          <>
+            <span className='text-gray-500 text-3xl'>"</span>
+            <p className='text-center'>{fact}</p>
+            <div className='flex justify-center'>
+              <button className='text-xs p-2 text-white round rounded-xl' style={{ backgroundColor: 'rgb(155,109,162)' }} onClick={updateRandomFact}>
+                Update random fact
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
