ee3e9ff47de65851c49a664801e97407af9d02e8
Victor Oliveira <victoroliveiraspk@gmail.com>
2022-06-12 12:33:10 -0300

    componentizes layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+interface Props {
+  children: ReactNode
+}
+
+const Layout = ({ children } : Props) => {
+  const router = useRouter()
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    const search = event.target.elements.search.value
+    router.push({
+      pathname: '/search',
+      query: { search }
+    })
+  }
+
+  return (
+    <>
+      <form className='flex flex-col items-center relative'
+        style={{ backgroundColor: 'rgb(155,109,162)' }}
+        onSubmit={handleSubmit}>
+        <Image src='/chuck-norris-push-up.gif' width={400} height={300}/>
+        <p className='absolute text-white text-3xl sm:text-5xl text-center'
+          style={{ top: '10%' }}>
+          Chuck Norris facts
+        </p>
+        <input className='shadow appearance-none border rounded w-10/12 sm:w-6/12 py-4 px-3 text-gray-700 focus:outline-none absolute'
+          placeholder='Search...'
+          style={{ bottom: '-25px' }}
+          name='search'
+          autoFocus
+          autoComplete='off'/>
+      </form>
+
+      <div className='mt-20 px-4'>
+        {children}
+      </div>
+    </> 
+  )
+}
+
+export default Layout
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import FactList from '../components/FactsList'
+import { useRouter } from 'next/router'
 
 const Search = () => {
+  const router = useRouter()
+  const search: string = router.query.search as string ?? ''
+
   return (
-    <></>
+    <Layout>
+      <FactList searchQuery={search} />
+    </Layout>
   )
 }
 
