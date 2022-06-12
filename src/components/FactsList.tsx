3bc414b1de71d24470d295fe60b96f3ee44b5bd5
Victor Oliveira <victoroliveiraspk@gmail.com>
2022-06-11 21:18:50 -0300

    fact list component refactoring

--- src/components/FactsList.tsx
+++ src/components/FactsList.tsx
@@ -1,19 +1,50 @@
+import { useEffect, useState } from "react"
 
 interface Props {
-  facts: string[]
   searchQuery: string
 }
 
-const FactsList = ({ facts, searchQuery }: Props) => {
+const searchFacts = (query: string) => {
+  return fetch(`https://api.chucknorris.io/jokes/search?query=${query}`).then(response => response.json())
+}
+
+const FactsList = ({ searchQuery }: Props) => {
+  const [loading, setLoading] = useState(false)
+  const [facts, setFacts] = useState([])
+
+  const updateList = () => {
+    if(searchQuery.length < 3) {
+      return setFacts([])
+    }
+    
+    setLoading(true)
+    searchFacts(searchQuery).then((response) => {
+      setFacts(
+        response.result.map((fact: any) => fact.value)
+      )
+    }).finally(() => setLoading(false))
+  }
+
+  useEffect(() => {
+    updateList()
+  }, [searchQuery])
+
   return (
-    <>
-      <p className='text-xs mb-5'>{facts.length} results for "{searchQuery}"</p>
-      {facts.map((fact) => (
-        <div className='border p-4 text-center'>
-          {fact}
-        </div>
-      ))}
-    </>
+    <div className='flex flex-col justify-around items-center'>
+      {
+        loading ? (<div className='loading'></div>)
+        : (<>
+            <div className='w-full'>
+              <p className='text-xs mb-5'>{facts.length} results for "{searchQuery}"</p>
+            </div>
+            {loading || facts.map((fact, index) => (
+              <div className='border p-4 w-full' key={index}>
+                {fact}
+              </div>
+            ))}
+        </>)
+      }
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,27 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import Image from 'next/image'
 import RandomFact from '../components/RandomFact'
 import FactsList from '../components/FactsList'
 
 const Home: NextPage = () => {
-  const facts = [
-    'Chuck Norris is the only person who can punch you in the back of the face.',
-    'While playing Blackjack, Chuck Norris can bust without hitting. He just chooses not to because Chuck Norris never busts',
-    'Chuck Norris once fed a cannibal with a knuckle sandwich.',
-    'Martial arts have given Chuck Norris the ability to predict your actions before you make them. He can look at you and know if you\'re getting ready to run, pull a gun or get your heart torn out. Guess what: You\'re always getting ready to get your heart torn out.',
-    'Albert Einstein\'s hair used to be neatly combed...until the day he met Chuck Norris.',
-    'the Speaking Clock calls up Chuck Norris to make sure IT is correct',
-    'Chuck Norris doesn\'t need help, help needs Chuck Norris.',
-  ]
+  const [searchQuery, setSearchQuery] = useState<string>('')
+
+  const onSubmitHandler = (event: any) => {
+    event.preventDefault()
+    setSearchQuery(event.target.elements.searchQuery.value)
+  }
 
   return (
     <>
-      <form className='flex flex-col items-center relative' style={{ backgroundColor: 'rgb(155,109,162)' }}>
+      <form className='flex flex-col items-center relative' style={{ backgroundColor: 'rgb(155,109,162)' }} onSubmit={onSubmitHandler}>
         <Image src='/chuck-norris-push-up.gif' width={400} height={300}/>
         <p className='absolute text-white text-5xl text-center' style={{ top: '10%' }}>Chuck Norris facts</p>
-        <input className='shadow appearance-none border rounded w-6/12 py-4 px-3 text-gray-700 focus:outline-none absolute' placeholder='Search...' style={{ bottom: '-25px' }}/>
+        <input className='shadow appearance-none border rounded w-6/12 py-4 px-3 text-gray-700 focus:outline-none absolute' placeholder='Search...' style={{ bottom: '-25px' }} name='searchQuery'/>
       </form>
 
       <div className='mt-20 px-4'>
-        {/* <RandomFact /> */}
-        <FactsList facts={facts} searchQuery='car' />
+        {searchQuery ? <FactsList searchQuery={searchQuery} /> : <RandomFact />}
       </div>
     </>
   )
