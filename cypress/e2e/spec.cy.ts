7b3c1eb32d3ee42d2a20f4aa36e6c7807d687c81
Victor Oliveira <victoroliveiraspk@gmail.com>
2022-06-12 15:13:12 -0300

    refactoring e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,36 +1,47 @@
 
 describe('home page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
-  
+
   it('show first random fact', () => {
     cy.get('.random-fact-text').should('not.be.empty')
   })
 
   it('update random fact', () => {
     cy.get('.update-random-fact-btn').click().then(() => {
+      cy.wait(1000)
       cy.get('.random-fact-text').should('not.be.empty')
     })
   })
 
-  it('search with results', () => {
-    cy.get('.search-input').type('cars{enter}')
+  it('redirect to search page', () => {
+    const query = 'test'
+    cy.get('.search-input').type(`${query}{enter}`)
+    cy.url().should('contain', `search?query=${query}`)
+  })
+})
+
+describe('search page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('with results', () => {
+    cy.get('.search-input').type(`cars{enter}`)
+    cy.wait(1000)
     cy.get('.fact-list').find('.fact-item').should('have.length.gte', 1)
   })
 
-  it('search with results', () => {
-    cy.get('.search-input').type('cars{enter}')
-    cy.get('.fact-list').find('.fact-item').should('have.length.gte', 1)
-  })
-
-  it('search without results', () => {
+  it('without results', () => {
     cy.get('.search-input').type('djlkasjdlas{enter}')
+    cy.wait(1000)
     cy.get('.fact-list').find('.fact-item').should('have.length', 0)
   })
 
-  it('search with less than 3 characters', () => {
+  it('query with less than 3 characters', () => {
     cy.get('.search-input').type('ca{enter}')
+    cy.wait(1000)
     cy.get('.fact-list').find('.fact-item').should('have.length', 0)
   })
 })
--- src/components/FactsList.tsx
+++ src/components/FactsList.tsx
@@ -8,9 +8,6 @@ interface Props {
 const FactsList = ({ facts, searchQuery }: Props) => {
   return (
     <div className='fact-list flex flex-col justify-around items-center'>
-      <div className='w-full'>
-        <p className='text-xs mb-5'>{facts.length} results for "{searchQuery}"</p>
-      </div>
       {facts.map((fact, index) => (
         <div className='fact-item border p-4 w-full' key={index}>
           {fact.value}
